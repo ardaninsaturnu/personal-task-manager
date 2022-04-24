b3f3032431f1863d24005635388987e786476caf
ardaninsaturnu <hsmclk2505@gmail.com>
2022-04-24 16:27:57 +0300

    :rocket: project created with next.js.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+const withSass = require('@zeit/next-sass');
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+module.exports = withSass({
+  cssModules: true
+})
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
